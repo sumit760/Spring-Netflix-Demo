a26c25e11c5320899b3e17e966acdda168344509
Sumit <pal.sumit.76@gmail.com>
2022-08-28 10:32:08 +0530

    Update 1

--- user-service/src/main/java/com/blueice/user/repository/UserRepository.java
+++ user-service/src/main/java/com/blueice/user/repository/UserRepository.java
@@ -3,10 +3,15 @@ package com.blueice.user.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.blueice.user.VO.ResponseTemplateVO;
 import com.blueice.user.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long>  {
+
+    ResponseTemplateVO getUserWithDepartmentById(User userId);
+
+    User findByUserId(User userId);
 
 
 }
